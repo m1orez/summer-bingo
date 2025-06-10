be3f6039e2781abad5c81bd628c6d2498b7a9430
M1orez <pauline@walhout-hia.nl>
2025-06-10 15:28:21 +0200

    no double usernames allowed

--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,3 @@
-
 import { auth, db } from "./firebaseConfig.js";
 import {
   createUserWithEmailAndPassword,
@@ -6,7 +5,11 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import {
   doc,
-  setDoc
+  setDoc,
+  collection,
+  query,
+  where,
+  getDocs
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const registerForm = document.getElementById("registerForm");
@@ -24,10 +27,19 @@ registerForm.addEventListener("submit", async (e) => {
   const password = document.getElementById("password").value;
 
   try {
+    const usersRef = collection(db, "users");
+    const q = query(usersRef, where("username", "==", username));
+    const querySnapshot = await getDocs(q);
+
+    if (!querySnapshot.empty) {
+      alert("Username is already taken. Please choose another one.");
+      return;
+    }
+
+
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     const user = userCredential.user;
 
-    // Store extra user data in Firestore
     await setDoc(doc(db, "users", user.uid), {
       username,
       email,
